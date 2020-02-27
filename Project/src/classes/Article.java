a32857b37094b4df63926afd2ad726ff9db60c2c
Chrisbelefantis <55947209+Chrisbelefantis@users.noreply.github.com>
2020-02-27 17:31:46 +0200

    Adding screenshots

--- Project/src/classes/Article.java
+++ Project/src/classes/Article.java
@@ -93,7 +93,7 @@ public class Article{
                    
                }
                 
-               return "error1";
+               return "No more space";
                 
                 
                 
